1d3108abdb79702b6bb288909ed3144eafdbf048
sapphi-red <green@sapphi.red>
2021-04-04 23:02:08 +0900

    Revert "Revert "Merge pull request #2 from traPtitech/invalid-tokens""
    This reverts commit be81b5e4f5eff8c575d70385416913d7b98d8916.

--- lib/AppInfo/TrapAuth.php
+++ lib/AppInfo/TrapAuth.php
@@ -7,6 +7,7 @@ use Exception;
 use OC\Security\CSRF\CsrfTokenManager;
 use OC_Util;
 use OCP\Files\IRootFolder;
+use OCP\IConfig;
 use OCP\IGroupManager;
 use OCP\IRequest;
 use OCP\IUserManager;
@@ -39,19 +40,23 @@ class TrapAuth {
     private IRequest $request;
     /** @var IUserSession */
     private IUserSession $session;
+    /** @var IConfig */
+    private IConfig $config;
 
     public function __construct(IUserManager $userManager,
                                 IGroupManager $groupManager,
                                 CsrfTokenManager $csrfTokenManager,
                                 IRootFolder $rootFolder,
                                 IRequest $request,
-                                IUserSession $session) {
+                                IUserSession $session,
+                                IConfig $config) {
         $this->userManager = $userManager;
         $this->groupManager = $groupManager;
         $this->csrfTokenManager = $csrfTokenManager;
         $this->rootFolder = $rootFolder;
         $this->request = $request;
         $this->session = $session;
+        $this->config = $config;
     }
 
     private function getUserInfo(): array {
@@ -59,6 +64,10 @@ class TrapAuth {
             $token = $this->request->getCookie("traP_token");
             if(is_null($token)){
                 throw new Exception("No token");
+            }
+            $invalidate_tokens = explode(" ", $this->config->getSystemValueString("trap.invalidate_tokens"));
+            if (in_array($token, $invalidate_tokens, true)) {
+                throw new Exception("Invalid token");
             }
             $jwt = JWT::decode($token, publicKey, array('RS256'));
         } catch(Exception $e) {
